ec670da0f29ebc19562f9eed82093ac55a65e734
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2019-09-28 08:36:24 -0500

    Begin API for new functionality

--- MyLeasing.Web/Controllers/API/OwnersController.cs
+++ MyLeasing.Web/Controllers/API/OwnersController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Common.Models;
 using MyLeasing.Web.Data;
 using MyLeasing.Web.Data.Entities;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Controllers.API
 {
